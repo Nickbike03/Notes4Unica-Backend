2addb20a430d698f9f415a4736553acd97f92a7a
Nickbike03 <dtmncl03c24c349x@studenti.unical.it>
2025-01-15 18:16:56 +0100

    initial commit

---
+++ src/test/java/com/unical/notes4unical/Notes4unicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unical.notes4unical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Notes4unicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
